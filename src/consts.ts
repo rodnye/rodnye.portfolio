f386982c108e86a0bbf7a80f1832c6ef4f87ce56
Rodny Estrada <rrodnyestrada1@gmail.com>
2026-05-08 19:42:09 -0400

    feat: personalize site title and add social media links

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,12 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
+export const SITE_TITLE = 'Rodny Estrada - Dev';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const GITHUB_LINK = "https://github.com/rodnye";
+export const LINKEDIN_LINK = "https://www.linkedin.com/in/rodny-estrada";
+export const X_LINK = "https://x.com/rodnyecode";
+export const GOOGLE_LINK = "https://www.google.com/search?q=rodny+estrada";
+export const TELEGRAM_LINK = "https://t.me/rodnyecode";
+export const EMAIL = "rrodnyestrada1@gmail.com";
